02308fe36aeda00ad2e7fe1e1448875e9a2497b7
Victoriya Roi <victoriyaroi2003@gmail.com>
2021-08-13 17:55:09 +0300

    Add 2-screen registration

--- app/src/main/java/com/example/mapsgps/LoginActivity.java
+++ app/src/main/java/com/example/mapsgps/LoginActivity.java
@@ -2,10 +2,13 @@ package com.example.mapsgps;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,19 +17,20 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class EmailPasswordActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-
     private EditText email_et, password_et;
-    private Button sign_in, sign_up;
+    private Button login_btn;
+    private TextView sign_up_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_email_password);
+        setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -46,45 +50,32 @@ public class EmailPasswordActivity extends AppCompatActivity {
         email_et = findViewById(R.id.email_et);
         password_et = findViewById(R.id.password_et);
 
-        sign_in = findViewById(R.id.sign_in);
-        sign_in.setOnClickListener(new View.OnClickListener() {
+        login_btn = findViewById(R.id.login);
+        login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signing(email_et.getText().toString(), password_et.getText().toString());
+                loginWithEmail(email_et.getText().toString(), password_et.getText().toString());
             }
         });
 
-        sign_up = findViewById(R.id.sign_up);
-        sign_up.setOnClickListener(new View.OnClickListener() {
+        sign_up_tv = findViewById(R.id.sign_up);
+        sign_up_tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registration(email_et.getText().toString(), password_et.getText().toString());
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
-
     }
 
-    private void signing(String email, String password){
+    private void loginWithEmail(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(EmailPasswordActivity.this, "Authorization is successful", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Authorization is successful", Toast.LENGTH_SHORT).show();
                 } else{
-                    Toast.makeText(EmailPasswordActivity.this, "Authorization is failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    private void registration(String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(EmailPasswordActivity.this, "Registration is successful", Toast.LENGTH_SHORT).show();
-                } else{
-                    Toast.makeText(EmailPasswordActivity.this, "Registration is failed", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Authorization is failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
---
+++ app/src/main/java/com/example/mapsgps/SignUpActivity.java
@@ -0,0 +1,65 @@
+package com.example.mapsgps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText email_et, password_et;
+    private Button sign_up_btn;
+    private TextView login_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email_et = findViewById(R.id.email_et);
+        password_et = findViewById(R.id.password_et);
+
+        login_tv = findViewById(R.id.login);
+        login_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        sign_up_btn = findViewById(R.id.sign_up);
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpWithEmail(email_et.getText().toString(), password_et.getText().toString());
+            }
+        });
+    }
+
+    private void signUpWithEmail(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignUpActivity.this, "Registration is successful", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(SignUpActivity.this, "Registration is failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
